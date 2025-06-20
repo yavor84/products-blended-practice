2d9681cc535ee66e738dcb1fc5f7067e1054bd61
Yavor <pojardim@gmail.com>
2025-06-20 23:13:11 +0300

    add 1 task

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import cors from 'cors';
-
 import { env } from './utils/env.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
-const PORT = Number(env('PORT', '3000'));
+const PORT = Number(env('PORT', '6060'));
 
 export const setupServer = () => {
   const app = express();
@@ -13,9 +14,9 @@ export const setupServer = () => {
 
   //   app.use(productsRouter);
 
-  //   app.use('*', notFoundHandler);
+  app.use(notFoundHandler);
 
-  //   app.use(errorHandler);
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
